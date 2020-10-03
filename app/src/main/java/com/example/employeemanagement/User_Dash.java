0db407f55b959a2dee1852b7b65b3d53e71edde8
Maheshi Purnima <62587305+maheshi98@users.noreply.github.com>
2020-10-03 17:40:17 +0530

    made changes

--- app/src/main/java/com/example/employeemanagement/User_Dash.java
+++ app/src/main/java/com/example/employeemanagement/User_Dash.java
@@ -9,8 +9,7 @@ import android.widget.Button;
 
 public class User_Dash extends AppCompatActivity {
 
-    Button leavebutton;
-    Button button;
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +17,6 @@ public class User_Dash extends AppCompatActivity {
         setContentView(R.layout.activity_user__dash);
 
 
-        leavebutton = findViewById(R.id.leavebtn);
-
-        leavebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Manage_Leave.class);
-              
-        button = findViewById(R.id.button);
-
-        button .setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(getApplicationContext(),UserProfile.class);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(getApplicationContext(),EmpDetails.class);
-
-                startActivity(i);
-            }
-        });
+       
     }
 }
