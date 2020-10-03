4e535f861af0178acbc7ee1993581bf757620ca8
Maheshi Purnima <62587305+maheshi98@users.noreply.github.com>
2020-10-03 17:02:04 +0530

    commented user_dash

--- app/src/main/java/com/example/employeemanagement/User_Dash.java
+++ app/src/main/java/com/example/employeemanagement/User_Dash.java
@@ -1,44 +1,44 @@
-package com.example.employeemanagement;
+// package com.example.employeemanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
+// import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+// import android.content.Intent;
+// import android.os.Bundle;
+// import android.view.View;
+// import android.widget.Button;
 
-public class User_Dash extends AppCompatActivity {
+// public class User_Dash extends AppCompatActivity {
 
-    Button leavebutton;
-    Button button;
+//     Button leavebutton;
+//     Button button;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user__dash);
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_user__dash);
 
 
-        leavebutton = findViewById(R.id.leavebtn);
+//         leavebutton = findViewById(R.id.leavebtn);
 
-        leavebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Manage_Leave.class);
-                startActivity(i);
+//         leavebutton.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 Intent i = new Intent(getApplicationContext(),Manage_Leave.class);
+//                 startActivity(i);
                 
               
-//         button = findViewById(R.id.button);
+// //         button = findViewById(R.id.button);
 
-//         button .setOnClickListener(new View.OnClickListener(){
-//             @Override
-//             public void onClick(View v){
-//                 Intent i = new Intent(getApplicationContext(),UserProfile.class);
-//         button.setOnClickListener(new View.OnClickListener(){
-//             @Override
-//             public void onClick(View v){
-//                 Intent i = new Intent(getApplicationContext(),EmpDetails.class);
+// //         button .setOnClickListener(new View.OnClickListener(){
+// //             @Override
+// //             public void onClick(View v){
+// //                 Intent i = new Intent(getApplicationContext(),UserProfile.class);
+// //         button.setOnClickListener(new View.OnClickListener(){
+// //             @Override
+// //             public void onClick(View v){
+// //                 Intent i = new Intent(getApplicationContext(),EmpDetails.class);
 
-            }
-        });
-    }
-}
+//             }
+//         });
+//     }
+// }
